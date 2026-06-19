88ff2a51f85c1b13055e3767280fb3298d7bebc5
Jitesh Kumar Viwhal <jiteshkumarviwhal@gmail.com>
2026-06-19 12:43:14 +0530

    Add various functions and array operations
    Added functions to check odd/even numbers, voting eligibility, and pass/fail status based on marks. Included array operations for fruits and names.

---
+++ practice.js
@@ -0,0 +1,99 @@
+let a = 10;
+function odd(){
+    if(a%2==0){
+        console.log("number is even");
+    }
+    else {
+  console.log("number is odd");
+    } }
+    odd();
+
+let age=20;
+function vote(){
+    if(age>=18){
+        console.log("you are eligible");
+    }
+else{
+    console.log("you are not eligible");
+}}
+vote();
+
+........................................................NEW CODE .............................................................................NEW CODE...............................................................
+
+let num=50;
+function marks(){
+    if(num>=50){
+        console.log("pass")
+    }
+    else{
+        console.log("fail")
+    }}
+    marks();
+
+...............................................................NEW CODE.............................................................................NEW CODE.......................................................
+
+let course=["html","css","javascript","react"];
+console.log(course.length);
+
+
+let fruits=["appple","mango","guava","banana"]
+// console.log(fruits.shift());
+// console.log(fruits);
+
+console.log(fruits.unshift("apple"));
+console.log(fruits);
+
+const name=["ravi","rahul","jitesh",]
+const surname=["machhal","kumar","dhillon",]
+console.log(name.concat(surname));
+
+let name=["ravi","jitesh","rahul","kapil",]
+let surname=["machhal","dhillon","kumar","sharma"]
+console.log(name.concat(surname));
+
+.............................................................NEW CODE.........................................................................NEW CODE.........................................................
+
+// let fruits=["apple","mango","banana",]
+// let season=["winter","summer","autumn"]
+// let months=["jan","fab","mar","april"]
+// console.log(fruits.concat(season,months));
+
+// let name=["ravi","ramma","rahul","jitesh",]
+// let sur=["machhal","bai","sharma","bhaman",]
+// console.log(name.concat(sur));
+// console.log(name.push("kapil"));
+// console.log(name);
+// console.log(name.pop(sur));
+// console.log(name)
+// console.log(name.shift());
+// console.log(name);
+// console.log(name.unshift(sur));
+// console.log(name)
+// const next = new Set(["ravi","ravi","ravi","ravi","ravi",]);
+// console.log(next);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
